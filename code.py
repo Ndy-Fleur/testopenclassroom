fbf962f203840e6dee8496e56bcfbfa1f64fc846
Fabio <camelifabio@gmail.com>
2019-05-29 16:34:00 +0200

    creation fichier code

---
+++ code.py
@@ -0,0 +1,17 @@
+num = int(input("enter a number: "))
+
+mod = num % 2
+
+if mod > 0:
+    print("is a odd number")
+else:
+    print("is a even number")
+
+check = int(input("give me a number to divide: "))
+
+if num % 4 == 0:
+        print(num, "is a multiple of 4")
+elif num % 2 == 0:
+        print(num, "is a even number")
+else:
+        print(num, "is a odd number")
